b5a8b387ddd0e9fa7a3d63a4f07c7525d0db732b
woonmong <kiata@naver.com>
2022-12-24 19:04:43 +0900

    make users model

--- houses/admin.py
+++ houses/admin.py
@@ -5,12 +5,7 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    
-    fields = (
-        "name",
-        "address",
-        ("price_per_night", "pets_allowed"),
-    )
+
 
     list_display = (
         "name",
@@ -23,13 +18,6 @@ class HouseAdmin(admin.ModelAdmin):
         "price_per_night",
         "pets_allowed",
     )
-
-    search_fields = (
-        "address__startswith",
-    )
-
-    list_display_links = ("name", "address")
-    list_editable = ("pets_allowed",)
 
     # exclude = [
     #     "price_per_night",
