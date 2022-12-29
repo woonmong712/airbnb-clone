a95b7e811588ae35cc4e78f011de7c75e5b6176d
woonmong <kiata@naver.com>
2022-12-29 18:58:53 +0900

    change data

--- users/admin.py
+++ users/admin.py
@@ -7,5 +7,39 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
-
+    # user page 내 상세
+    fieldsets = (
+        (
+            "Profile", 
+            {
+                "fields": ("username","password","name","email","is_host"),
+                "classes":("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields" : (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields":(
+                    "last_login",
+                    "date_joined",
+                ),
+                "classes":(
+                    "collapse",
+                ),
+            },
+        ),
+    )
+    # user list 구성
+    list_display = ("username","email","name","is_host")
